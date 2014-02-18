a20e0efa7883085258bf48e9e3933eaf9bd131c4
Charlie Cheever <ccheever@gmail.com>
2014-02-17 17:06:04 -0800

    First cut

---
+++ index.js
@@ -0,0 +1,24 @@
+var es = require('event-stream');
+var preresolve = require('preresolve');
+var stream = require('stream');
+
+module.exports = function() {
+  var doReplace = function(file, callback) {
+    var isStream = file.contents && typeof file.contents.on === 'function' && typeof file.contents.pipe === 'function';
+    var isBuffer = Buffer.isBuffer(file.contents);
+
+    if (isStream) {
+      return callback(new Error('gulp-preresolve: Cannot do preresolution replace on a stream'), file);
+    }
+
+    if (isBuffer) {
+        file.contents = preresolve(file);
+      }
+      return callback(null, file);
+    }
+
+    callback(null, file);
+  };
+
+  return es.map(doReplace);
+};
